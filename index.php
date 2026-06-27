7a8d0cba971a7888b3f3a602e34ea755a87e4adb
rosaputri <rosaeka72@gmail.com>
2026-06-28 01:51:15 +0700

    Update sistem inventory PT Pena Jaya

--- index.php
+++ index.php
@@ -31,7 +31,8 @@
     <div class="login-container">
         <div class="card card-primary card-outline mb-4">
             <div class="card-header text-center">
-                <h3 class="card-title float-none">Inventory System</h3>
+            <img src="admin/assets/img/penajaya.png" width="80" height="80" alt="icon">
+                <h3 class="card-title float-none">Pena Jaya</h3>
             </div>
             <form action="login.php" method="post">
                 <div class="card-body">
